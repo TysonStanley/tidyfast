652c4e78691fac4fa1909caf3e1330ed8a1ebe02
TysonStanley <t.barrett@aggiemail.usu.edu>
2019-11-01 13:15:32 -0600

    Update fill.R

--- R/fill.R
+++ R/fill.R
@@ -6,7 +6,6 @@
 #' @param dt the data table (or if not a data.table then it is coerced with as.data.table)
 #' @param ... the columns to fill
 #' @param id the grouping variable(s) to fill within
-#' @param order should the table be ordered first? default is NULL (which assumes the table is already in the right order). Other option is to indicate variable(s) to order by.
 #' @param .direction either "down" or "up" (down fills values down, up fills values up), or "downup" (down first then up) or "updown" (up first then down)
 #'
 #' @examples
@@ -27,7 +26,7 @@
 #' @importFrom data.table as.data.table
 #'
 #' @export
-dt_fill <- function(dt, ..., id = NULL, order = NULL, .direction = c("down", "up", "downup", "updown")){
+dt_fill <- function(dt, ..., id = NULL, .direction = c("down", "up", "downup", "updown")){
 
   if (isFALSE(is.data.table(dt)))
     dt <- data.table::as.data.table(dt)
@@ -41,15 +40,8 @@ dt_fill <- function(dt, ..., id = NULL, order = NULL, .direction = c("down", "up
 
   dots  <- paste_dots(...)
   by    <- substitute(id)
-  order <- substitute(order)
 
-  if (isFALSE(is.null(order))){
-    dt <- dt[order(eval(order)), lapply(.SD, fun), by = eval(by), .SDcols = dots]
-  } else {
-    dt <- dt[, lapply(.SD, fun), by = eval(by), .SDcols = dots]
-  }
-
-  dt
+  dt[, lapply(.SD, fun), by = eval(by), .SDcols = dots]
 }
 
 paste_dots <- function(...){

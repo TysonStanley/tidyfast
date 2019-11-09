e872071672eeaf4e4eb7023b970ce931ede498c4
TysonStanley <t.barrett@aggiemail.usu.edu>
2019-11-09 14:15:40 -0700

    count with wt

--- R/count.R
+++ R/count.R
@@ -5,6 +5,7 @@
 #' @param dt the data table to uncount
 #' @param ... groups
 #' @param na.rm should any rows with missingness be removed before the count? Default is `FALSE`.
+#' @param wt the wt assigned to the counts (same number of rows as the data)
 #'
 #' @examples
 #'
@@ -12,25 +13,31 @@
 #' dt <- data.table(
 #'   x = rnorm(1e5),
 #'   y = runif(1e5),
-#'   grp = sample(1L:3L, 1e5, replace = TRUE)
+#'   grp = sample(1L:3L, 1e5, replace = TRUE),
+#'   wt = runif(1e5, 1, 100)
 #'   )
 #'
 #' dt_count(dt, grp)
 #' dt_count(dt, grp, na.rm = TRUE)
+#' dt_count(dt, grp, na.rm = TRUE, wt = wt)
 #'
 #' @import data.table
 #' @importFrom stats complete.cases
 #'
 #' @export
-dt_count <- function(dt, ..., na.rm = FALSE){
+dt_count <- function(dt, ..., na.rm = FALSE, wt = NULL){
 
   if (isFALSE(is.data.table(dt)))
     dt <- as.data.table(dt)
 
   dots <- substitute(list(...))
+  wt <- substitute(wt)
 
   if (na.rm)
     dt <- dt[complete.cases(dt)]
+
+  if (!is.null(wt))
+    return(dt[, list(N = sum(eval(wt))), keyby = eval(dots)])
 
   dt[, .N, keyby = eval(dots)]
 }

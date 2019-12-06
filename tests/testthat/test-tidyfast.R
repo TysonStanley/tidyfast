1e2fab04683c1916a4d0988b5ea721093c7d9105
TysonStanley <t.barrett@aggiemail.usu.edu>
2019-12-06 00:41:43 -0700

    updated docs

--- tests/testthat/test-tidyfast.R
+++ tests/testthat/test-tidyfast.R
@@ -11,6 +11,8 @@ test_that("dt_case_when works", {
 
   expect_equal(names(table(cased)), c("high", "low"))
   expect_error(dt_case_when(x < median(x), 1))
+  expect_error(dt_case_when(x < median(x), 1,
+                            x >= median(x), 2))
 
 })
 

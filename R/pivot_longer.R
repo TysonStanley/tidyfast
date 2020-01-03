8c49a0cfe456aeefb734ce73b6798a8d3f211ed8
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-01-03 15:39:25 -0600

    Improve column_selector()

--- R/pivot_longer.R
+++ R/pivot_longer.R
@@ -80,12 +80,17 @@ dt_pivot_longer.default <- function(dt_,
 column_selector <- function(dt_, select_vars) {
 
   data_names <- colnames(dt_)
-  data_vars <- setNames(as.list(seq_along(dt_)), data_names)
-  select_index <- unlist(eval(select_vars, data_vars))
 
-  if (any(select_index > 0) && any(select_index < 0))
-    stop("cols must only contain columns to drop OR columns to add, not both")
+  data_vars <- setNames(as.list(seq_along(dt_)), data_names)
+
+  select_index <- unlist(eval(select_vars, data_vars))
+  keep_index <- unique(select_index[select_index > 0])
+  if (length(keep_index) == 0) keep_index <- seq_along(dt_)
+  drop_index <- unique(abs(select_index[select_index < 0]))
+
+  select_index <- setdiff(keep_index, drop_index)
 
   select_vars <- data_names[select_index]
+
   select_vars
 }

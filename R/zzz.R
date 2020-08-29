f78dfc0ffe5baf29c1874f74eddaa2865c36c628
Tyson Barrett <t.barrett@aggiemail.usu.edu>
2020-08-28 22:43:59 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onLoad <- function(libname = find.package("furniture"), pkgname = "furniture"){
+.onLoad <- function(libname = find.package("tidyfast"), pkgname = "tidyfast"){
   if(getRversion() >= "2.15.1") {
     utils::globalVariables(c("..others", ".", "..keep"))
   }

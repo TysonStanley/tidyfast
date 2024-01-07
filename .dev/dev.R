07faa3ee25aeaf1278b5a2dca745ee03d971f69e
Tyson Barrett <t.barrett88@gmail.com>
2024-01-07 13:56:43 -0700

    update github actions

--- .dev/dev.R
+++ .dev/dev.R
@@ -3,5 +3,9 @@ cpp11::cpp_register(here::here())
 ## clean up by removing all .o files
 files = fs::dir_ls(here::here("src/"), regexp = "\\.o|\\.so")
 fs::file_delete(files)
+# document
 devtools::document(here::here())
+# check package
 devtools::check(here::here())
+# fix github actions
+usethis::use_github_action()
